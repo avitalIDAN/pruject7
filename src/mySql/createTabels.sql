35be77798286abec1176f19b8eed9c659000da6b
sapirvanunu <svanunu@g.jct.ac.il>
2023-07-26 14:46:47 +0300

    create DB

---
+++ src/mySql/createTabels.sql
@@ -0,0 +1,102 @@
+CREATE TABLE users (
+  id INT PRIMARY KEY,
+  name VARCHAR(255),
+  username VARCHAR(255),
+  email VARCHAR(255),
+  address VARCHAR(255),
+  phone VARCHAR(255)
+);
+
+CREATE TABLE userspass (
+  id INT PRIMARY KEY,
+  username VARCHAR(255),
+  password VARCHAR(255),
+  isManager boolean,
+ FOREIGN KEY (id) REFERENCES users(id)
+);
+
+CREATE TABLE lending(
+id INT PRIMARY KEY,
+userId INT,
+username VARCHAR(255),
+itemId INT,
+tableName VARCHAR(255),
+dateLending date,
+dateReturn date,
+isLending boolean,
+isReturn boolean,
+managerId INT,
+FOREIGN KEY (userId) REFERENCES users(id),
+FOREIGN KEY (managerId) REFERENCES users(id)
+);
+
+CREATE TABLE donation(
+id INT PRIMARY KEY,
+userId INT,
+username VARCHAR(255),
+itemName VARCHAR(255),
+tableName VARCHAR(255),
+isDonated boolean,
+FOREIGN KEY (userId) REFERENCES users(id)
+);
+
+CREATE TABLE garden(
+  id INT PRIMARY KEY,
+  name VARCHAR(255),
+  quantity INT,
+  quantityAvailable INT,
+  cost INT,
+  size INT,
+  image VARCHAR(255)
+);
+
+
+CREATE TABLE electric(
+  id INT PRIMARY KEY,
+  name VARCHAR(255),
+  quantity INT,
+  quantityAvailable INT,
+  cost INT,
+  size INT,
+  image VARCHAR(255)
+);
+
+CREATE TABLE handTools(
+  id INT PRIMARY KEY,
+  name VARCHAR(255),
+  quantity INT,
+  quantityAvailable INT,
+  cost INT,
+  size INT,
+  image VARCHAR(255)
+);
+
+CREATE TABLE potectorTools(
+  id INT PRIMARY KEY,
+  name VARCHAR(255),
+  quantity INT,
+  quantityAvailable INT,
+  cost INT,
+  size INT,
+  image VARCHAR(255)
+);
+
+CREATE TABLE homeTools(
+  id INT PRIMARY KEY,
+  name VARCHAR(255),
+  quantity INT,
+  quantityAvailable INT,
+  cost INT,
+  size INT,
+  image VARCHAR(255)
+);
+
+CREATE TABLE camping(
+  id INT PRIMARY KEY,
+  name VARCHAR(255),
+  quantity INT,
+  quantityAvailable INT,
+  cost INT,
+  size INT,
+  image VARCHAR(255)
+);
