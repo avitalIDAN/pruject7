73cab7a1939d18fc0b00611b203e568ad2c4c9bb
אביטל עי <tchyk52@gmail.com>
2023-08-01 00:48:10 +0300

    lending

--- src/pages/ListLendings.js
+++ src/pages/ListLendings.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { 
+  requestsGet,
+  requestsPut, 
+} from "../requestsToServer.js";
 import { useTable } from 'react-table';
 
+const tableName = 'lending';
+
 const TableWithCheckboxes = () => {
+  const [data, setData] = useState([]);
+
+  async function fetchData() {
+    try {
+      const response = await requestsGet(`/`+tableName);
+      console.log(response);
+      setData(response);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   // Sample data for demonstration
-  const data = React.useMemo(
-    () => [
-      { id: 1, name: 'Item 1', price: 10 },
-      { id: 2, name: 'Item 2', price: 20 },
-      { id: 3, name: 'Item 3', price: 30 },
-      // Add more rows as needed
-    ],
-    []
-  );
+  // const data = React.useMemo(
+  //   () => fetchData(),
+  //   []
+  // );
 
   // Define columns for the table
   const columns = React.useMemo(
@@ -19,17 +35,48 @@ const TableWithCheckboxes = () => {
       { Header: 'ID', accessor: 'id' },
       { Header: 'שם', accessor: 'username' },
     //   { Header: 'שם מוצר', accessor: 'price' },
-      { Header: 'סוג מוצר', accessor: 'price' },
-      { Header: 'תאריך החזרה', accessor: 'date' },
-    ],
-    []
+      { Header: 'סוג מוצר', accessor: 'tableName' },
+      { Header: 'תאריך השאלה', accessor: 'dateLending' },
+      { Header: 'תאריך החזרה', accessor: 'dateReturn' },      
+      { Header: 'הושאל', accessor: 'checkbox1', 
+      Cell: ({ row }) => <input type="checkbox" checked={row.original.isReturn} onChange={() => handleCheckbox1Click(row.original)} /> },
+      { Header: 'הוחזר', accessor: 'checkbox2', 
+      Cell: ({ row }) => <input type="checkbox" checked={row.original.isLending} onChange={() => handleCheckbox2Click(row.original)} /> }
+      ],
+      []
   );
 
+  const handleCheckbox1Click = async (Lending) => {
+    const dateLending = new Date();
+    Lending.dateLending = dateLending.toDateString();
+    dateLending.setDate(dateLending.getDate() + 7);
+    Lending.dateReturn = dateLending.toDateString();
+    Lending.isLending = true;
+    console.log(Lending);
+    //requestsPut();
+    const response = await requestsPut(`/${tableName}/${Lending.id}`,Lending);
+    //d.toDateString();
+    //עדכון
+    //תאריך*2
+    //console.log(`Checkbox ${checkboxNumber} for ID ${id} clicked.`);
+    // Add your custom logic here, based on the checkbox click
+  };
+  const handleCheckbox2Click = async (Lending) => {
+    Lending.isLending = true;
+    console.log(Lending);
+    //requestsPut();
+    const response = await requestsPut(`/${tableName}/${Lending.id}`,Lending);
+    //עדכון
+    //כמות
+    //console.log(`Checkbox ${checkboxNumber} for ID ${id} clicked.`);
+    // Add your custom logic here, based on the checkbox click
+  };
+
   // Add checkboxes to the columns array for the last two columns
-  columns.push(
-    { Header: 'Checkbox 1', accessor: 'הושאל', Cell: ({ row }) => <input type="checkbox" /> },
-    { Header: 'Checkbox 2', accessor: 'הוחזר', Cell: ({ row }) => <input type="checkbox" /> }
-  );
+  // columns.push(
+  //   { Header: 'Checkbox 1', accessor: 'הושאל', Cell: ({ row }) => <input type="checkbox" /> },
+  //   { Header: 'Checkbox 2', accessor: 'הוחזר', Cell: ({ row }) => <input type="checkbox" /> }
+  // );
 
   const {
     getTableProps,
